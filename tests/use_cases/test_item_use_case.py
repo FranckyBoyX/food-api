983b813b09132adf5f9d1958ccfd900764aa11c7
franck <franck_x@hotmail.com>
2018-11-25 13:24:40 -0500

    Added a test for the list use case

--- tests/use_cases/test_item_use_case.py
+++ tests/use_cases/test_item_use_case.py
@@ -1,24 +1,24 @@
-from unittest import TestCase
+import time
+from unittest import TestCase, mock
+
+from food_api.domain.food_item import FoodItem
+from food_api.use_cases.item_use_case import ItemUseCase
 
 
 class TestItemUseCase(TestCase):
+
+    def setUp(self):
+        repo = mock.Mock()
+
+        food_item1 = FoodItem("food item 1", 3, int(time.time()))
+        food_item2 = FoodItem("food item 2", 5, int(time.time()))
+        self.food_list = [food_item1, food_item2]
+
+        repo.list.return_value = self.food_list
+        self.my_item_use_case = ItemUseCase(repo)
+
     def test_list_items(self):
-        self.fail()
+        items = self.my_item_use_case.list_items()
 
-    def test_increment_item_count(self):
-        self.fail()
+        self.assertListEqual(self.food_list, items)
 
-    def test_decrement_item_count(self):
-        self.fail()
-
-    def test_change_description(self):
-        self.fail()
-
-    def test_add_item(self):
-        self.fail()
-
-    def test_remove_item(self):
-        self.fail()
-
-    def test__update_if_not_depleted(self):
-        self.fail()
